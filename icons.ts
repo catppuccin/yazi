9bfdccc2b78d7493fa5c5983bc176a0bc5fef164
uncenter <47499684+uncenter@users.noreply.github.com>
2024-06-16 22:37:59 -0400

    feat: theme file icons (#4)

---
+++ icons.ts
@@ -0,0 +1,50 @@
+import { closest } from "ctpvert";
+import { type ColorName, flavorEntries, flavors } from "@catppuccin/palette";
+
+const { icons_by_filename, icons_by_file_extension } = JSON.parse(
+  Deno.args.join("")!,
+) as Record<
+  string,
+  Record<
+    string,
+    {
+      name: string;
+      icon: string;
+      cterm_color: string;
+      color: string;
+    }
+  >
+>;
+
+for (const [identifier] of flavorEntries) {
+  let output = `[icon]\nprepend_rules = [\n`;
+
+  for (
+    const [filename, { color, icon }] of Object.entries(icons_by_filename)
+      .sort(([a], [b]) => a < b ? -1 : a > b ? 1 : 0)
+  ) {
+    const match = closest(color);
+    output += `	{ name = "${filename}", text = "${icon}", fg = "${
+      flavors[identifier].colors[match[identifier].name as ColorName].hex
+    }" },\n`;
+  }
+
+  for (
+    const [ext, { color, icon }] of Object.entries(icons_by_file_extension)
+      .sort(([a], [b]) => a < b ? -1 : a > b ? 1 : 0)
+  ) {
+    const match = closest(color);
+    output += `	{ name = "*.${ext}", text = "${icon}", fg = "${
+      flavors[identifier].colors[match[identifier].name as ColorName].hex
+    }" },\n`;
+  }
+
+  output += "]\n";
+
+  const dist = `themes/${identifier}.toml`;
+  const theme = await Deno.readTextFile(dist);
+  await Deno.writeTextFile(
+    dist,
+    theme + "\n" + output,
+  );
+}
