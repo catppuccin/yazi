90310d5f80a36bc66479aecb412b55b2b86162f6
uncenter <47499684+uncenter@users.noreply.github.com>
2024-08-01 11:37:27 -0400

    fix: use custom stringification for inline dicts in arrays

--- icons.ts
+++ icons.ts
@@ -1,6 +1,5 @@
 import { closest } from "ctpvert";
 import { type ColorName, flavorEntries, flavors } from "@catppuccin/palette";
-import { stringify } from "smol-toml";
 
 const { icons_by_filename, icons_by_file_extension } = JSON.parse(
   await Deno.readTextFile("./icons.json"),
@@ -17,45 +16,31 @@ const { icons_by_filename, icons_by_file_extension } = JSON.parse(
   >
 >;
 
-type IconEntry = {
-  name: string;
-  text: string;
-  fg_dark: string;
-  fg_light: string;
-};
-
 for (const [identifier] of flavorEntries) {
-  const output = {
-    icon: {
-      files: [] as IconEntry[],
-      exts: [] as IconEntry[],
-    },
-  };
+  let output = "[icon]\n\n";
 
   const addIcons = (
     icons: Record<string, { color: string; icon: string }>,
-    output: IconEntry[],
+    name: string,
   ) => {
+    output += `${name} = [\n`;
     for (const [key, { color, icon }] of Object.entries(icons)) {
       const match = closest(color);
       const fg =
         flavors[identifier].colors[match[identifier].name as ColorName].hex;
-      output.push({
-        name: key,
-        text: icon,
-        fg_dark: fg,
-        fg_light: fg,
-      });
+      output +=
+        `  { name = "${key}", text = "${icon}", fg_dark = "${fg}", fg_light = "${fg}" },\n`;
     }
+    output += "]\n";
   };
 
-  addIcons(icons_by_filename, output.icon.files);
-  addIcons(icons_by_file_extension, output.icon.exts);
+  addIcons(icons_by_filename, "files");
+  addIcons(icons_by_file_extension, "exts");
 
   const dist = `themes/${identifier}.toml`;
   const theme = await Deno.readTextFile(dist);
   await Deno.writeTextFile(
     dist,
-    theme + "\n" + stringify(output),
+    theme + "\n" + output,
   );
 }
